c510b6e8690a828e9c4d46711d5be0a6776b1a06
runesam <same7.hamada@icloud.com>
2016-04-19 10:18:19 +0300

    react-skeleton

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require ('./componentes/List.jsx');
+
+ReactDOM.render(<List></List>, document.getElementById('yarabeshfeh'));
